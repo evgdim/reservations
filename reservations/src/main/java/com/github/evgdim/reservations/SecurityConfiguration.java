44b413a8089ed377c9facfe22bbc5d5856c01bfb
Evgeni DImitrov <evgeni.dimitrov@nevexis.com>
2019-01-11 16:43:22 +0200

    add user details service

--- reservations/src/main/java/com/github/evgdim/reservations/SecurityConfiguration.java
+++ reservations/src/main/java/com/github/evgdim/reservations/SecurityConfiguration.java
@@ -3,6 +3,9 @@ package com.github.evgdim.reservations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
@@ -21,6 +24,17 @@ public class SecurityConfiguration {
             .build();
     }
     
+    
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+         UserDetails user = User.withDefaultPasswordEncoder()
+              .username("user")
+              .password("password")
+              .roles("USER")
+              .build();
+         return new MapReactiveUserDetailsService(user);
+    }
+
     
    
 }
