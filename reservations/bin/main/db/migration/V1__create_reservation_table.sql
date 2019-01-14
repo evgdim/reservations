5fbd7b526c72707caa488c752a1707c1c3e63271
Evgeni DImitrov <evgeni.dimitrov@nevexis.com>
2019-01-14 11:37:58 +0200

    ignore .gradle and bin

--- reservations/bin/main/db/migration/V1__create_reservation_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE reservation (
-  id IDENTITY PRIMARY KEY,
-  description VARCHAR(50) NOT NULL, 
-  detailed_description VARCHAR(500),
-  start TIMESTAMP,
-  end TIMESTAMP,
-  user_id bigint,
-  resource_id bigint
-);
