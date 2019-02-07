9ca7985bf6d3f2645b02eabe5a8598ed391bfa28
Evgeni DImitrov <evgeni.dimitrov@nevexis.com>
2019-02-07 13:48:09 +0200

    switched to bootstrap

--- reservations-react/src/App.tsx
+++ reservations-react/src/App.tsx
@@ -12,14 +12,25 @@ class App extends Component {
   render() {
     return (
       <div>
-        <nav className="uk-navbar-container uk-margin" uk-navbar="mode: click">
-          <div className="uk-navbar-center">
-            <ul className="uk-navbar-nav">
-              <li className="uk-active"><Link to="/">Home</Link></li>
-              <li><Link to="/tasks">Reservations</Link></li>
-              <li><Link to="/new">New</Link></li>
-            </ul>
-
+        
+              {/* <Link to="/">Home</Link>
+              <Link to="/tasks">Reservations</Link>
+              <Link to="/new">New</Link> */}
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              {/* <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-item nav-link" href="#">Features</a>
+              <a className="nav-item nav-link" href="#">Pricing</a>
+              <a className="nav-item nav-link disabled" href="#">Disabled</a> */}
+              <Link to="/" className="nav-item nav-link active">Home</Link>
+              <Link to="/tasks" className="nav-item nav-link">Reservations</Link>
+              <Link to="/new" className="nav-item nav-link">New</Link>
+            </div>
           </div>
         </nav>
 
