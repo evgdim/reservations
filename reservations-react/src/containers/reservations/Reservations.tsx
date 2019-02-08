78ebc9263b50d8dd967486e8d6f66e49dd0d5828
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2019-02-08 21:30:12 +0200

    rewrite to functional component with hook

--- reservations-react/src/containers/reservations/Reservations.tsx
+++ reservations-react/src/containers/reservations/Reservations.tsx
@@ -1,72 +1,41 @@
-// import * as React from 'react';
-
-// export interface ReservationsProps {
-// }
-
-// export interface ReservationsState {
-//   reservations: any
-// }
-
-// export default class Reservations extends React.Component<ReservationsProps, ReservationsState> {
-//   constructor(props: ReservationsProps) {
-//     super(props);
-//     this.state = {
-//       reservations: []
-//     }
-//   }
-  
-  
-//   componentDidMount() {
-//     fetch("http://localhost:8080/reservations?name=asd", { method:'GET', headers: new Headers({'Authorization':'Basic ' + btoa("user:password")})})
-//     .then(res => res.json())
-//     .then(data => {
-//       this.setState({
-//         reservations: data
-//       })
-//     })
-//     .catch(err => console.error(err))
-//   }
-
-//   public render() {
-//     let reservationsList = this.state.reservations.map((r: any) => 
-//       <tr key={r.id}>
-//         <th scope="row"></th>
-//         <td>{r.description}</td>
-//         <td>-</td>
-//         <td>-</td>
-//       </tr>);
-//     return (
-//       <div>
-//         <table className="table">
-//         <thead>
-//           <tr>
-//             <th scope="col">#</th>
-//             <th scope="col">Description</th>
-//             <th scope="col">Start</th>
-//             <th scope="col">End</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {reservationsList}
-//         </tbody>
-//         </table>
-//       </div>
-
-//     );
-//   }
-// }
-
 import * as React from 'react';
 
 export interface ReservationsProps {
 }
 
 export default function Reservations (props: ReservationsProps) {
-    const [count, setCount] = React.useState(1);
+    const [data, setData] = React.useState({ reservations: [] });
+    React.useEffect(() => {
+      fetch("http://localhost:8080/reservations?name=asd", { method:'GET', headers: new Headers({'Authorization':'Basic ' + btoa("user:password")})})
+      .then(res => res.json())
+      .then(resp => {
+        console.log(resp);
+        setData({reservations: resp});
+      })
+      .catch(err => console.error(err))
+    }, []);
     return (
       <div>
-        Clicks: {count}
-        <button onClick={() => setCount(count + 1)}>Click Me</button>
+        <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Description</th>
+            <th scope="col">Start</th>
+            <th scope="col">End</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.reservations.map((r: any) => 
+                    <tr key={r.id}>
+                      <th scope="row"></th>
+                      <td>{r.description}</td>
+                      <td>{r.start}</td>
+                      <td>{r.end}</td>
+                    </tr>  
+          )}
+        </tbody>
+        </table>
       </div>
     );
 }
