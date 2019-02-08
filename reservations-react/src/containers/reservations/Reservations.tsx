3097baae782022ab83168ec12b0975eb8b82310f
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2019-02-08 20:14:08 +0200

    refactor components to pure functions

--- reservations-react/src/containers/reservations/Reservations.tsx
+++ reservations-react/src/containers/reservations/Reservations.tsx
@@ -1,57 +1,72 @@
+// import * as React from 'react';
+
+// export interface ReservationsProps {
+// }
+
+// export interface ReservationsState {
+//   reservations: any
+// }
+
+// export default class Reservations extends React.Component<ReservationsProps, ReservationsState> {
+//   constructor(props: ReservationsProps) {
+//     super(props);
+//     this.state = {
+//       reservations: []
+//     }
+//   }
+  
+  
+//   componentDidMount() {
+//     fetch("http://localhost:8080/reservations?name=asd", { method:'GET', headers: new Headers({'Authorization':'Basic ' + btoa("user:password")})})
+//     .then(res => res.json())
+//     .then(data => {
+//       this.setState({
+//         reservations: data
+//       })
+//     })
+//     .catch(err => console.error(err))
+//   }
+
+//   public render() {
+//     let reservationsList = this.state.reservations.map((r: any) => 
+//       <tr key={r.id}>
+//         <th scope="row"></th>
+//         <td>{r.description}</td>
+//         <td>-</td>
+//         <td>-</td>
+//       </tr>);
+//     return (
+//       <div>
+//         <table className="table">
+//         <thead>
+//           <tr>
+//             <th scope="col">#</th>
+//             <th scope="col">Description</th>
+//             <th scope="col">Start</th>
+//             <th scope="col">End</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {reservationsList}
+//         </tbody>
+//         </table>
+//       </div>
+
+//     );
+//   }
+// }
+
 import * as React from 'react';
 
 export interface ReservationsProps {
 }
 
-export interface ReservationsState {
-  reservations: any
-}
-
-export default class Reservations extends React.Component<ReservationsProps, ReservationsState> {
-  constructor(props: ReservationsProps) {
-    super(props);
-    this.state = {
-      reservations: []
-    }
-  }
-  
-  
-  componentDidMount() {
-    fetch("http://localhost:8080/reservations?name=asd", { method:'GET', headers: new Headers({'Authorization':'Basic ' + btoa("user:password")})})
-    .then(res => res.json())
-    .then(data => {
-      this.setState({
-        reservations: data
-      })
-    })
-    .catch(err => console.error(err))
-  }
-
-  public render() {
-    let reservationsList = this.state.reservations.map((r: any) => 
-      <tr key={r.id}>
-        <th scope="row"></th>
-        <td>{r.description}</td>
-        <td>-</td>
-        <td>-</td>
-      </tr>);
+export default function Reservations (props: ReservationsProps) {
+    const [count, setCount] = React.useState(1);
     return (
       <div>
-        <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Description</th>
-            <th scope="col">Start</th>
-            <th scope="col">End</th>
-          </tr>
-        </thead>
-        <tbody>
-          {reservationsList}
-        </tbody>
-        </table>
+        Clicks: {count}
+        <button onClick={() => setCount(count + 1)}>Click Me</button>
       </div>
-
     );
-  }
 }
