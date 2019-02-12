7ecd4cce6ae7e969b1385fb98d28a2f386c49783
Evgeni DImitrov <evgeni.dimitrov@nevexis.com>
2019-02-12 14:25:35 +0200

    add mapper tests

--- reservations/src/test/java/com/github/evgdim/reservations/ReservationMapperTests.java
+++ reservations/src/test/java/com/github/evgdim/reservations/ReservationMapperTests.java
@@ -2,12 +2,16 @@ package com.github.evgdim.reservations;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.github.evgdim.reservations.model.Reservation;
+import com.github.evgdim.reservations.model.dto.ReservationDto;
 import com.github.evgdim.reservations.model.mappers.ReservationMapper;
 import com.github.evgdim.reservations.model.mappers.ReservationMapperImpl;
 
@@ -20,5 +24,37 @@ public class ReservationMapperTests {
 	@Test
 	public void reservationDtoToReservation_shouldReturnNull_whenNullIsPassed() {
 		assertThat(reservationMapper.reservationDtoToReservation(null)).isNull();
+	}
+	
+	@Test
+	public void reservationDtoToReservation_shouldMap() {
+		LocalDateTime start = LocalDateTime.now();
+		ReservationDto dto = getDto(start);
+		Reservation res = reservationMapper.reservationDtoToReservation(dto);
+		assertThat(res.getDescription()).isEqualTo("desc");
+		assertThat(res.getStart()).isEqualTo(start);
+		assertThat(res.getUser().getId()).isEqualTo(456L);
+	}
+	
+	@Test
+	public void reservationDtoToReservation_shouldMapResourceAndUserToNull_whenMissingInDto() {
+		LocalDateTime start = LocalDateTime.now();
+		ReservationDto dto = getDto(start);
+		dto.setUserId(null);
+		dto.setResourceId(null);
+		Reservation res = reservationMapper.reservationDtoToReservation(dto);
+		assertThat(res.getUser()).isNull();
+		assertThat(res.getResource()).isNull();
+	}
+	
+	private ReservationDto getDto(LocalDateTime start) {
+		ReservationDto dto = new ReservationDto();
+		dto.setDescription("desc");
+		dto.setDetailedDescription("detDesc");
+		dto.setStart(start);
+		dto.setEnd(start.plusDays(1));
+		dto.setResourceId(123L);
+		dto.setUserId(456L);
+		return dto;
 	}
 }
